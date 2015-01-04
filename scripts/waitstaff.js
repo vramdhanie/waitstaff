fb7aaa1421c6439aa9963a2ef063767a946ba6b5
Vincent Ramdhanie <vramdhanie@gmail.com>
2015-01-04 15:55:20 -0400

    Add content and style to html page

--- scripts/waitstaff.js
+++ scripts/waitstaff.js
@@ -1,4 +1,12 @@
 angular.module('waitApp', [])
   .controller('WaitController', function($scope){
-    $scope.boo = "who?";
+    $scope.price = 0;
+    $scope.tax = 0;
+    $scope.tip = 0;
+    $scope.tipAmount = 0;
+    $scope.subtotal = 0;
+    $scope.total = 0;
+    $scope.tipTotal = 0;
+    $scope.mealCount = 0;
+    $scope.tipPerMeal = 0;
   });
